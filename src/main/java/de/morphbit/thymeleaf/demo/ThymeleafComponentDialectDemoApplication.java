92c8e97bb0d82d51071eba15b3ede332997d15ce
Danny Rottstegge <d.rottstegge@evosec.de>
2017-09-05 13:10:45 +0200

    Rename package

--- src/main/java/de/morphbit/thymeleaf/demo/ThymeleafComponentDialectDemoApplication.java
+++ src/main/java/de/morphbit/thymeleaf/demo/ThymeleafComponentDialectDemoApplication.java
@@ -1,4 +1,4 @@
-package de.morphbit.thymeleafcomponentdialectdemo;
+package de.morphbit.thymeleaf.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
