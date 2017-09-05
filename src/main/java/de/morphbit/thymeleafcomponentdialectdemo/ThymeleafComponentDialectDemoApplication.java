3a15a81ecae6e933bce3e26afd79d456fecf4795
Danny Rottstegge <d.rottstegge@evosec.de>
2017-09-05 11:58:38 +0200

    First in

---
+++ src/main/java/de/morphbit/thymeleafcomponentdialectdemo/ThymeleafComponentDialectDemoApplication.java
@@ -0,0 +1,12 @@
+package de.morphbit.thymeleafcomponentdialectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafComponentDialectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafComponentDialectDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/de/morphbit/thymeleafcomponentdialectdemo/ThymeleafComponentDialectDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package de.morphbit.thymeleafcomponentdialectdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafComponentDialectDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
