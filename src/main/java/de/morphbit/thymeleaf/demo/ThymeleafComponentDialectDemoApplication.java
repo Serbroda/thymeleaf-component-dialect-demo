26f1bd4e367fb33696442125aca7c3bfee31aeff
Danny Rottstegge <d.rottstegge@evosec.de>
2017-09-08 14:05:25 +0200

    Add test components

--- src/main/java/de/morphbit/thymeleaf/demo/ThymeleafComponentDialectDemoApplication.java
+++ src/main/java/de/morphbit/thymeleaf/demo/ThymeleafComponentDialectDemoApplication.java
@@ -1,8 +1,11 @@
 package de.morphbit.thymeleaf.demo;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,7 +18,9 @@ public class ThymeleafComponentDialectDemoApplication {
 	}
 
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("msg", "Hello world " + new Date());
+		model.addAttribute("html", "<span th:text=\"${msg}\"></span>");
 		return "index";
 	}
 }
