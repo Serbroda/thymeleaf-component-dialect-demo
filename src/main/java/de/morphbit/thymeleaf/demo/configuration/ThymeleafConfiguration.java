b41ff21b47ae6fdcd28386056fc00bfcb5e1c474
Serbroda <danny@rottstegge.net>
2017-09-09 11:29:49 +0200

    Register custom components

--- src/main/java/de/morphbit/thymeleaf/demo/configuration/ThymeleafConfiguration.java
+++ src/main/java/de/morphbit/thymeleaf/demo/configuration/ThymeleafConfiguration.java
@@ -1,5 +1,8 @@
 package de.morphbit.thymeleaf.demo.configuration;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,6 +11,7 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import de.morphbit.thymeleaf.dialect.ComponentDialect;
+import de.morphbit.thymeleaf.model.ThymeleafComponent;
 
 @Configuration
 @EnableWebMvc
@@ -18,7 +22,15 @@ public class ThymeleafConfiguration extends WebMvcConfigurerAdapter {
 	        ITemplateResolver templateResolver) {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver);
-		templateEngine.addDialect(new ComponentDialect());
+		templateEngine.addDialect(new ComponentDialect(thymeleafComponents()));
 		return templateEngine;
+	}
+	
+	@Bean
+	public Set<ThymeleafComponent> thymeleafComponents() {
+		Set<ThymeleafComponent> components = new HashSet<>();
+		components.add(new ThymeleafComponent("link", "components/link :: link"));
+		components.add(new ThymeleafComponent("panel", "components/panel :: panel"));
+		return components;
 	}
 }
