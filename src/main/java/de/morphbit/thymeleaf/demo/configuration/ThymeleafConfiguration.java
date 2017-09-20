03c619aefa028cb8c8a669359d7a98cd1a35a9aa
Danny Rottstegge <d.rottstegge@evosec.de>
2017-09-20 14:01:30 +0200

    Add parser to dialect configuration

--- src/main/java/de/morphbit/thymeleaf/demo/configuration/ThymeleafConfiguration.java
+++ src/main/java/de/morphbit/thymeleaf/demo/configuration/ThymeleafConfiguration.java
@@ -1,44 +1,20 @@
 package de.morphbit.thymeleaf.demo.configuration;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import de.morphbit.thymeleaf.dialect.ComponentDialect;
-import de.morphbit.thymeleaf.model.ThymeleafComponent;
+import de.morphbit.thymeleaf.parser.StandardThymeleafComponentParser;
 
 @Configuration
 public class ThymeleafConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
-	public SpringTemplateEngine templateEngine(
-	        ITemplateResolver templateResolver) {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver);
-		templateEngine.addDialect(new ComponentDialect(thymeleafComponents()));
-		return templateEngine;
-	}
-
-	@Bean
-	public Set<ThymeleafComponent> thymeleafComponents() {
-		Set<ThymeleafComponent> components = new HashSet<>();
-		components
-		    .add(new ThymeleafComponent("link", "components/link :: link"));
-		components
-		    .add(new ThymeleafComponent("panel", "components/panel :: panel"));
-		components.add(new ThymeleafComponent("button-group",
-		    "components/buttongroup :: buttongroup"));
-		components.add(
-		    new ThymeleafComponent("button", "components/button :: button"));
-		components.add(
-			    new ThymeleafComponent("floating", "components/floatingLabel :: floating"));
-		components.add(
-			    new ThymeleafComponent("console", "components/console :: console"));
-		return components;
+	public ComponentDialect componentDialect() {
+		ComponentDialect dialect = new ComponentDialect();
+		dialect.addParser(
+		    new StandardThymeleafComponentParser("templates/", ".html", "components"));
+		return dialect;
 	}
 }
